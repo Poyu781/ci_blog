2754f0491ee88fb4972bf6c325242b7bbd3818b6
Poyu781 <v3708599@gmail.com>
2021-07-27 10:17:03 +0800

    set default database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '34.146.36.62',
+	'username' => 'airflow_remo',
+	'password' => 'password',
+	'database' => 'ciblog',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
