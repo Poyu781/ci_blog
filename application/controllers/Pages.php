411ac3ceaea48675e91c13a93a47f0b976c554ed
Poyu781 <v3708599@gmail.com>
2021-07-27 10:04:24 +0800

    set default url and base views

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,5 +1,6 @@
 <?php
 	class Pages extends CI_Controller{
+
 		public function view($page = 'home'){
 			if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
 				show_404();
@@ -10,5 +11,10 @@
 			$this->load->view('templates/header');
 			$this->load->view('pages/'.$page, $data);
 			$this->load->view('templates/footer');
-		}
+        }
+        public function index(){
+            $this->load->view('templates/header');
+			$this->load->view('pages/home');
+			$this->load->view('templates/footer');
+        }
 	}
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,22 @@
 <html>
-    <head>
-        <title>Blog</title>
-    </head>
-    <body>
+
+<head>
+	<title>ciBlog</title>
+	<link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+</head>
+
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+			</div>
+
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+				</ul>
+			</div>
+        </div>
+	</nav>
+	<div class="container">
