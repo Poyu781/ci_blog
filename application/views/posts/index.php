97b403bc8312893964f7253cf1a9d4313bd060aa
Poyu781 <v3708599@gmail.com>
2021-07-29 01:44:01 +0800

    design posts front-end base view

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,5 +1,21 @@
 <h2><?= $title ?></h2>
-<?php foreach($posts as $post) : ?>
-	<h3><?php echo $post['title']; ?></h3>
-	<div><?php echo $post['body']; ?></div>
-<?php endforeach; ?>
+<section class="post__section">
+	<h3>title</h3>
+		<div class="row">
+			<div class="col-md-9">
+				<small class="post-date">Posted on: </strong></small><br>
+			ariticlefdsf
+			<br><br>
+			</div>
+		</div>	
+
+	<h3>title</h3>
+		<div class="row">
+			<div class="col-md-9">
+				<small class="post-date">Posted on: </strong></small><br>
+			ariticlefdsf
+			<br><br>
+			</div>
+		</div>
+</section>
+<script src="<?php echo site_url('assets/js/posts.js'); ?>" type="text/javascript"></script>
