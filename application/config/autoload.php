414c8952c927fad5818aa6baddbb07e7fb83dcf9
Poyu781 <v3708599@gmail.com>
2021-07-29 10:57:37 +0800

    finish register page

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session');
+$autoload['libraries'] = array('database','session','form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('posts_model','comments_model');
+$autoload['model'] = array('posts_model','comments_model','user_model');
---
+++ application/controllers/Users.php
@@ -0,0 +1,49 @@
+<?php
+	class Users extends CI_Controller{
+		// Register user
+		public function register(){
+
+			$this->form_validation->set_rules('username', 'Username', 'required|callback_check_username_exists');
+			$this->form_validation->set_rules('email', 'Email', 'required|callback_check_email_exists');
+			$this->form_validation->set_rules('password', 'Password', 'required');
+			$this->form_validation->set_rules('password2', 'Confirm Password', 'matches[password]');
+
+			if($this->form_validation->run() === FALSE){
+				$this->load->view('templates/header');
+				$this->load->view('users/register');
+				$this->load->view('templates/footer');
+			} else {
+				// Encrypt password
+                $enc_password = md5($this->input->post('password'));
+                $username = $this->input->post('username');
+                $this->user_model->register($enc_password);
+                $user_id = $this->user_model->login($username, $enc_password);
+                $user_data = array(
+                    'user_id' => $user_id,
+                    'username' => $username,
+                    'logged_in' => true
+                );
+                $this->session->set_userdata($user_data);
+				redirect('posts');
+			}
+        }
+        		// Check if username exists
+		public function check_username_exists($username){
+			$this->form_validation->set_message('check_username_exists', 'That username is taken. Please choose a different one');
+			if($this->user_model->check_username_exists($username)){
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		// Check if email exists
+		public function check_email_exists($email){
+			$this->form_validation->set_message('check_email_exists', 'That email is taken. Please choose a different one');
+			if($this->user_model->check_email_exists($email)){
+				return true;
+			} else {
+				return false;
+			}
+		}
+    }
